cff304675a55effec5fc60fcabd065e956b7a58b
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-05-06 01:32:53 +0600

    first

--- app/src/main/java/com/daud/dailyexpensefire/MainActivity.java
+++ app/src/main/java/com/daud/dailyexpensefire/MainActivity.java
@@ -17,14 +17,18 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLay;
     private NavigationView navDr;
+    private BottomNavigationView navBtm;
     private ImageButton toggle;
 
     @Override
@@ -34,10 +38,55 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLay = findViewById(R.id.drawerLay);
         navDr = findViewById(R.id.navDr);
+        navBtm = findViewById(R.id.navBtm);
         toggle = findViewById(R.id.toogle);
 
         toggle.setOnClickListener(view -> {
             drawerLay.openDrawer(GravityCompat.START);
+        });
+
+        navDr.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.eName:
+                        Toast.makeText(MainActivity.this, "Edit Name", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.sOut:
+                        Toast.makeText(MainActivity.this, "Sign Out", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.dash:
+                        Toast.makeText(MainActivity.this, "Dash Board", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.addex:
+                        Toast.makeText(MainActivity.this, "Add Expense", Toast.LENGTH_SHORT).show();
+                        break;
+
+                        default:break;
+                }
+                return true;
+            }
+        });
+
+        navBtm.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.dashBtm:
+                        Toast.makeText(MainActivity.this, "Dash Board", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.addexBtm:
+                        Toast.makeText(MainActivity.this, "Add Expense", Toast.LENGTH_SHORT).show();
+                        break;
+
+                        default:break;
+                }
+                return true;
+            }
         });
     }
 }

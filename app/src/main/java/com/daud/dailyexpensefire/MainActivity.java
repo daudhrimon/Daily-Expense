05f36ceacf7a3be91ca08003190275566a01067c
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-05-06 01:19:24 +0600

    first

--- app/src/main/java/com/daud/dailyexpensefire/MainActivity.java
+++ app/src/main/java/com/daud/dailyexpensefire/MainActivity.java
@@ -1,22 +1,43 @@
 package com.daud.dailyexpensefire;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLay;
+    private NavigationView navDr;
+    private ImageButton toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLay = findViewById(R.id.drawerLay);
+        navDr = findViewById(R.id.navDr);
+        toggle = findViewById(R.id.toogle);
+
+        toggle.setOnClickListener(view -> {
+            drawerLay.openDrawer(GravityCompat.START);
+        });
     }
 }
